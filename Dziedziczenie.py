621aa3cecc9c3559f11e75a87e40fa420ee3947a
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-07-24 11:27:07 +0200

    problems with appending quantity

--- Dziedziczenie.py
+++ Dziedziczenie.py
@@ -1,4 +1,3 @@
-from typing_extensions import Self
 from faker import Faker
 fake = Faker("en_US")
 
@@ -18,24 +17,7 @@ class BaseContact:
     def label_lenght(self):
         return self._label_lenght
 
-def create_contacts(kind, quantity):
-        while True:
-            choice = input("Enter choice (p/b/x):")
-
-            if choice in ('p', 'b',):
-                quantity = float(input("Enter number of cards:"))
-       
-            if choice == 'p':
-                contacts.append(human_1)
-            elif choice == 'b':
-                business_contacts.append(human_1)
-            elif choice == 'x':
-                exit()
-            else:
-                print("Invalid input")
-
-if __name__ == "__main__":
-    class BusinessContact(BaseContact):
+class BusinessContact(BaseContact):
         def __init__(self, business_tel, company, position, *args, **kwargs):
             super().__init__(*args, **kwargs)
             self.business_tel = business_tel
@@ -44,20 +26,34 @@ if __name__ == "__main__":
 
         def contact(self):
             return f'I am dialing number {self.business_tel} and calling {self.name} {self.surname}'
-      
+contacts = []
+business_contacts = []
+def create_contacts():
+        while True:
+            choice = input("Enter choice (p/b/x):")
+
+            if choice in ('p', 'b',):
+                quantity = float(input("Enter number of cards:"))
+       
+            if choice == 'p':
+                contacts.append(human_1*quantity)
+            elif choice == 'b':
+                business_contacts.append(human_1*quantity)
+            elif choice == 'x':
+                exit()
+            else:
+                print("Invalid input")
+
+if __name__ == "__main__":  
     human_1 = BusinessContact(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(),
               email_address=fake.email(), tel=fake.phone_number(), business_tel=fake.phone_number())
 
-    def create_contacts(kinds, quantity):
-        return create_contacts
-    
-    contacts = []
-    business_contacts = []
     print("Select the type of business card:")
     print("p - private")
     print("b - business")
     print("x - exit programm")
     
+    create_contacts()
     
     print(human_1.contact())
     print(human_1.label_lenght)
