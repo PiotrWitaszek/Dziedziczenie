2d676136a62c46e359a16d1dc950c93dc57cde68
Piotr Witaszek <p.a.witaszek@gmail.com>
2022-07-29 19:02:07 +0200

    Hopefully final version

--- Dziedziczenie.py
+++ Dziedziczenie.py
@@ -2,7 +2,7 @@ from faker import Faker
 fake = Faker("en_US")
 
 class BaseContact:
-    def __init__(self, name, surname, email_address,tel):
+    def __init__(self, name, surname, email_address, tel):
         self.name = name
         self.surname = surname
         self.email_address = email_address
@@ -13,8 +13,12 @@ class BaseContact:
     def contact(self):
         return f'I am dialing number {self.tel} and calling {self.name} {self.surname}'  
 
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.email_address} {self.tel}{self.business_tel} {self.company} {self.position}'
 
-  
+    def __repr__(self):
+        return f'{self.name} {self.surname} {self.email_address} {self.tel}'
+    
     @property
     def label_lenght(self):
         return self._label_lenght
@@ -43,7 +47,7 @@ def create_contacts():
             if choice == 'p':
                 contacts = []
                 for i in range(quantity):
-                    contacts.append([fake.first_name(), fake.last_name(), fake.email(), fake.phone_number()])
+                    contacts.append(BaseContact(name=fake.first_name(), surname=fake.last_name(), email_address=fake.email(), tel=fake.phone_number()))
                 print(contacts)
             elif choice == 'b':
                 business_contacts = []
@@ -56,6 +60,4 @@ def create_contacts():
                 print("Invalid input")
 
 if __name__ == "__main__":  
-    human_1 = BusinessContact(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(),
-              email_address=fake.email(), tel=fake.phone_number(), business_tel=fake.phone_number())
     create_contacts()
